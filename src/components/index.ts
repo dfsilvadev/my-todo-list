bc0247c9ce5aecf8ab62b270add78bfa8cd0cd04
Daniel Silva <dfsilva.dxp@gmail.com>
2023-07-03 18:25:50 -0300

    feat: este commit adiciona o componente form.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,6 @@
 export { default as Button } from "./Button";
+export { default as Container } from "./Container";
+export { default as Form } from "./Form";
 export { default as Header } from "./Header";
 export { default as Input } from "./Input";
 export { default as Logo } from "./Logo";
